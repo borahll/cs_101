d54f4b7783c2344706255c0d5edf13e284a46241
Bora Haliloglu <91379730+borahll@users.noreply.github.com>
2021-10-02 13:19:27 +0300

    Add files via upload

---
+++ labs/lab1/Lab01_Q2.java
@@ -0,0 +1,31 @@
+public class Lab01_Q2 {
+
+    public static void main(String[] args) {
+
+        // declaring the expressions
+
+        double eq1 = (32.2 - (1.7 / 2.2)) / ((1.5- 7.3) * (4.3 + 2.4));          
+        double eq2 = ((73.5 * 16.4 - Math.pow(3,6)) / (34 + Math.pow(2, 5)));
+        double eq3 = (Math.pow(1.2 + 0.8, -1/3) );
+
+
+        //First expression
+
+        System.out.print("Result of the first expression is : "); 
+        System.out.println(eq1);
+
+
+        //second expresiion
+
+        System.out.print("Result of the second expression is : ");   
+        System.out.println(eq2);
+        
+
+        //third expression
+
+        System.out.print("Result of the third expression is : ");   
+        System.out.println(eq3);
+
+    }
+    
+}
---
+++ labs/lab1/Lab01_Q3.java
@@ -0,0 +1,24 @@
+public class Lab01_Q3 {
+    public static void main(String[] args) {
+        
+         final int totalEarthSurfaceArea = 510072000; //earth total area
+         final double areaOfTurkey = 0.1536; //00         //Turkey total area
+         double water = 70.8;                //00         //Water percentage of Earth
+         double land = 29.2;                 //00         //Land percentage of Earth
+         double turkeyWetLand = 1.3;         //0000         //water percentage of Turkey
+         double turkeyDryLand = 98.7;        //0000
+
+
+        System.out.println("Earth has " + (totalEarthSurfaceArea*land)/100 + " km2 dry land and "  + (totalEarthSurfaceArea*water)/100 +" km2 water." );   //First expression
+
+        System.out.println("Turkey has " + (totalEarthSurfaceArea*turkeyDryLand*areaOfTurkey)/10000 + " km2 dry land and " + (totalEarthSurfaceArea*areaOfTurkey*turkeyWetLand)/10000 + " km2 water.");    //Second expression
+
+        System.out.println("Turkey has " + ((((totalEarthSurfaceArea*areaOfTurkey*turkeyDryLand)/10000) / (totalEarthSurfaceArea*land)/100)/100) + " percent of the Earth's dry land.");        //Third expression
+
+        System.out.println("Turkey has " + ((((totalEarthSurfaceArea*areaOfTurkey*turkeyWetLand)/10000) / (totalEarthSurfaceArea*water)/100)/100) + " percent of the Earth's wet land.");       //Fourth expression
+
+        System.out.println("breakpoint control");
+                                 
+    }
+    
+}
