15d255b5863f65decdafac73f679c7ae73f67bdf
Bhll <bora.haliloglu@ug.bilkent.edu.tr>
2021-10-09 12:13:16 +0300

    cs_101 2. lab 3

--- labs/lab2/Lab02_Q3.java
+++ labs/lab2/Lab02_Q3.java
@@ -1,7 +1,44 @@
 package lab2;
 
+import java.util.Scanner;
+
+import java.lang.String;
+
 public class Lab02_Q3 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the title:");
+        String title = sc.nextLine();
+
+
+        System.out.println("------------------------------------------");
+
+
+        int num_Of_Characters = title.length();
+        System.out.println("Number of characters is:" + num_Of_Characters);
+
+        System.out.println("All uppercase:\t" + title.toUpperCase());
+
+        System.out.println("Remove leading and trailing blank charecters:" + title.trim());
+
+//      char char_At = sc.next().charAt(0);
+
+        System.out.println("Charecter at index position 5:" + title.charAt(5));
+
+        String x_Y = title.substring(2,7);
+        System.out.println("Substring from 3rd character(inclusive) to 8th character(exclusive):" + x_Y);
         
+/*      System.out.print("\nEnter the Character to Find =  ");
+		char ch = sc.next().charAt(0);
+*/
+        char a = 'a';
+        System.out.println("First occurence of character 'a':" + title.indexOf(a));
+        System.out.print("Last occurence of character 'a':" + title.lastIndexOf(a) + "\n");
+
+
+
+        sc.close();
     }
 }
